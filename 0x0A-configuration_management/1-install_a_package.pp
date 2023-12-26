1a9490bdec471dc8d33edcbcfd4cc7420c68d1af
”osmmf” <elmeftahioussama@gmail.com>
2023-12-26 13:58:41 -0800

    “commit”

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,8 @@
+#!/usr/bin/pup
+# This Puppet manifest installs Flask version 2.1.0 using pip3.
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
